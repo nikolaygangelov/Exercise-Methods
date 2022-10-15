19d41f19ceab5187fc473cde932d7c4f623c5d49
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-10-15 16:27:29 +0300

    Add files via upload

---
+++ Vowels Count.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _2._Vowels_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+
+            string toLower = input.ToLower();
+            int sumOfVowels = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if(toLower[i]=='a' || toLower[i]=='e' || toLower[i] == 'i' || toLower[i] == 'o' || toLower[i] == 'u')
+                {
+                    sumOfVowels++;
+                }
+            }
+            Console.WriteLine(sumOfVowels);
+        }
+    }
+}
